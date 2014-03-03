4313cd3c0c0f53d78a4e096aa854559bbefd6a93
Joel Chelliah <joel.chelliah@bekk.no>
2014-03-03 19:35:25 +0100

    moving more things around to fit heroku project tree format

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 elm snake.elm --make --build-dir=build --runtime=./elm-runtime.js
-cp $HASKELL_HOME/ghc-7.6.3/lib/Elm-0.11/share/elm-runtime.js ./build/elm-runtime.js
+cp $HASKELL_HOME/ghc-7.6.3/lib/Elm-0.11/share/elm-runtime.js ./elm-runtime.js
 mv ./build/snake.html ./build/home.html
